972aeef0f330c128c05f678136801c97361a58a3
Eva-Zaitseva <evik.zcv@gmail.com>
2026-02-13 23:55:11 +0600

    Add files via upload

--- components/core.php
+++ components/core.php
@@ -5,10 +5,8 @@ $username = "root";
 $password = "";
 $dbname = "movies_db";
 
-// Создаем соединение
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Проверяем соединение
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
